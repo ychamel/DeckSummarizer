2ddf2d0d25cedbed35a41d3444a99e52f1f67cff
ychamel <ychamel@hotmail.com>
2024-01-10 15:14:52 +0200

    updated analytics format

--- Main/core/Analytics.py
+++ Main/core/Analytics.py
@@ -1,7 +1,16 @@
+import datetime
+import uuid
+
 import requests
 import json
 
 import streamlit as st
+
+
+@st.cache_data
+def get_id():
+    return str(uuid.uuid4())
+
 
 def set_data(query, answer):
     url = "https://eu-west-2.aws.data.mongodb-api.com/app/data-iruhp/endpoint/data/v1/action/findOne"
@@ -10,10 +19,11 @@ def set_data(query, answer):
         "collection": "Synapse",
         "database": "DataAnalytics",
         "dataSource": "Cluster0",
-        "projection": {
-            "_id": 1,
+        "document": {
+            "id": get_id(),
             "question": query,
-            "answer": answer
+            "answer": answer,
+            "time": str(datetime.datetime.now())
         }
     })
     headers = {
@@ -22,6 +32,3 @@ def set_data(query, answer):
         'api-key': st.session_state.get("MONGODB_API_KEY"),
     }
     response = requests.request("POST", url, headers=headers, data=payload)
-
-
-
